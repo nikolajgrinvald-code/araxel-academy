949cd83f4a69b933140834fed6ceb9378555a3fe
Nik <nikolajgrinvald@gmail.com>
2026-07-08 01:10:43 +0500

    add static root for render

--- academy/settings.py
+++ academy/settings.py
@@ -80,6 +80,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static', BASE_DIR / 'academy/static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
