d50a4889aa211cf82e33a58a10c08d5f8bc91940
Nik <nikolajgrinvald@gmail.com>
2026-07-08 13:45:14 +0500

    add yandex object storage config

--- academy/settings.py
+++ academy/settings.py
@@ -83,4 +83,15 @@ STATICFILES_DIRS = [BASE_DIR / 'static', BASE_DIR / 'academy/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+if os.environ.get('YANDEX_ACCESS_KEY_ID'):
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_ACCESS_KEY_ID = os.environ.get('YANDEX_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('YANDEX_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('YANDEX_BUCKET_NAME', 'araxel-academy-media')
+    AWS_S3_ENDPOINT_URL = os.environ.get('YANDEX_S3_ENDPOINT_URL', 'https://storage.yandexcloud.net')
+    AWS_S3_REGION_NAME = os.environ.get('YANDEX_S3_REGION_NAME', 'ru-central1')
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_FILE_OVERWRITE = False
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
