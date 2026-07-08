cc3795e63753524e7252ffb4ad7682e66e60dc46
Nik <nikolajgrinvald@gmail.com>
2026-07-08 17:55:25 +0500

    fix 500: avoid import-time boto3 crash in urls.py

--- academy/urls.py
+++ academy/urls.py
@@ -2,14 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_GET
-from django.http import FileResponse, Http404
-import boto3
 from users import views as user_views
 
 
-@require_GET
 def media_proxy(request, path):
     if not path:
         raise Http404()
@@ -26,18 +21,21 @@ def media_proxy(request, path):
     elif ext == 'png':
         mime = 'image/png'
 
-    client = boto3.client(
-        's3',
-        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        endpoint_url=getattr(settings, 'AWS_S3_ENDPOINT_URL', None),
-        region_name=getattr(settings, 'AWS_S3_REGION_NAME', 'us-east-1'),
-    )
     try:
+        import boto3
+        client = boto3.client(
+            's3',
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            endpoint_url=getattr(settings, 'AWS_S3_ENDPOINT_URL', None),
+            region_name=getattr(settings, 'AWS_S3_REGION_NAME', 'us-east-1'),
+        )
         obj = client.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=path)
-        return FileResponse(obj['Body'], content_type=obj.get('ContentType', mime))
     except Exception:
         raise Http404()
+
+    from django.http import FileResponse, Http404
+    return FileResponse(obj['Body'], content_type=obj.get('ContentType', mime))
 
 
 urlpatterns = [
